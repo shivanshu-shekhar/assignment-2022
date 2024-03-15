b724f6703c369698b843a0737cd88adbeb49e0b6
shivanshu-shekhar <119838421+shivanshu-shekhar@users.noreply.github.com>
2024-03-15 17:22:22 +0530

    Add files via upload

---
+++ todo7/scripts.js
@@ -0,0 +1,69 @@
+var selectedRow = null;
+
+function onFormSubmit() {
+  if (validate()) {
+    var formData = readFormData();
+    if (selectedRow == null) insertNewRecord(formData);
+    else updateRecord(formData);
+    resetForm();
+  }
+}
+
+function readFormData() {
+  var formData = {};
+  formData["fullName"] = document.getElementById("fullName").value;
+  formData["email"] = document.getElementById("email").value;
+  return formData;
+}
+
+function insertNewRecord(data) {
+  var table = document
+    .getElementById("employeeList")
+    .getElementsByTagName("tbody")[0];
+  var newRow = table.insertRow(table.length);
+  cell1 = newRow.insertCell(0);
+  cell1.innerHTML = data.fullName;
+  cell2 = newRow.insertCell(1);
+  cell2.innerHTML = data.email;
+  cell3 = newRow.insertCell(2);
+  cell3.innerHTML = `<a class="edit-btn" onClick="onEdit(this)">Edit</a>
+  <a class="delete-btn" onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+  document.getElementById("fullName").value = "";
+  document.getElementById("email").value = "";
+  selectedRow = null;
+}
+
+function onEdit(td) {
+  selectedRow = td.parentElement.parentElement;
+  document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+  document.getElementById("email").value = selectedRow.cells[1].innerHTML;
+}
+function updateRecord(formData) {
+  selectedRow.cells[0].innerHTML = formData.fullName;
+  selectedRow.cells[1].innerHTML = formData.email;
+}
+
+function onDelete(td) {
+  row = td.parentElement.parentElement;
+  document.getElementById("employeeList").deleteRow(row.rowIndex);
+  resetForm();
+}
+function validate() {
+  isValid = true;
+  if (document.getElementById("fullName").value == "") {
+    isValid = false;
+    document.getElementById("fullNameValidationError").classList.remove("hide");
+  } else {
+    isValid = true;
+    if (
+      !document
+        .getElementById("fullNameValidationError")
+        .classList.contains("hide")
+    )
+      document.getElementById("fullNameValidationError").classList.add("hide");
+  }
+  return isValid;
+}
